e982f1361f5f0c330d978d2be05736ded71c3c84
gongchen <gongchen@usc.edu>
2017-08-02 10:43:40 +0800

    54 local component

---
+++ LearnVueJs/VueCliIntro/src/Components/Info.vue
@@ -0,0 +1,3 @@
+<template>
+    <p>Important information.</p>
+</template>
--- LearnVueJs/VueCliIntro/src/Components/User.vue
+++ LearnVueJs/VueCliIntro/src/Components/User.vue
@@ -2,15 +2,20 @@
     <div id="user">
         <h3>The User</h3>
         <p>My user name is {{username}}</p>
+        <app-info></app-info>
     </div>
 </template>
 
 <script>
+    import Info from './Info.vue'
     export default {
         data () {
             return {
                 username: 'Max'
             }
+        },
+        components: {
+            'app-info': Info
         }
     }
 </script>
