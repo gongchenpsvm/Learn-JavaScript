1016df1d85cf07ea67cd9afab026e1e9bc5f2ebc
gongchen <gongchen@usc.edu>
2017-08-02 12:57:49 +0800

    58 add router links on home page

--- LearnVueJs/VueCliIntro/src/App.vue
+++ LearnVueJs/VueCliIntro/src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="app">
-<!--
-      <app-user>
-    </app-user>
--->
+    <div>
+        <router-link to="/">Home</router-link>
+        <router-link to="/blog">Blog</router-link>
+        <router-link to="/account">Account</router-link>
+    </div>
     <router-view></router-view>
     <p>Test scoped style of User.vue</p>
   </div>
