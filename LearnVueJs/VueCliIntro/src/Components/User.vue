eb2a0b04004bd36221faef7955a97e7954f699cc
gongchen <gongchen@usc.edu>
2017-08-02 10:18:01 +0800

    53. global component

---
+++ LearnVueJs/VueCliIntro/src/Components/User.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="user">
+        <h3>The User</h3>
+        <p>My user name is {{username}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                username: 'Max'
+            }
+        }
+    }
+</script>
+
+<style>
+    p {
+        font-style: italic;
+    }
+</style>
